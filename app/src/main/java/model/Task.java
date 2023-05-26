1e046dbf1215935530bb6038f0beeb888c87dd3d
Camila Zucchi <camilazucchi@outlook.com>
2023-05-26 17:50:34 -0300

    feat: add initial project

---
+++ app/src/main/java/model/Task.java
@@ -0,0 +1,106 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Task {
+    
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private boolean isCompleted;
+    private String notes;
+    private LocalDate deadline;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    public Task(int id, int idProject, String name, String description, boolean isCompleted, String notes, LocalDate deadline, LocalDate createdAt, LocalDate updatedAt) {
+        this.id = id;
+        this.idProject = idProject;
+        this.name = name;
+        this.description = description;
+        this.isCompleted = isCompleted;
+        this.notes = notes;
+        this.deadline = deadline;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+    public void setIdProject(int idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" + "id=" + id + ", idProject=" + idProject + ", name=" + name + ", description=" + description + ", isCompleted=" + isCompleted + ", notes=" + notes + ", deadline=" + deadline + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + '}';
+    }
+    
+}
