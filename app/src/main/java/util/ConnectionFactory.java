e93cea5c08e949cc65dfe44654423a0717172104
Camila Zucchi <camilazucchi@outlook.com>
2023-05-29 14:10:31 -0300

    chore: update mysql database connection parameters

--- app/src/main/java/util/ConnectionFactory.java
+++ app/src/main/java/util/ConnectionFactory.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package util;
 
-/**
- *
- * @author camil
- */
 public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.j";
+    public static final String URL = "";
+    public static final String USER = "root";
+    public static final String PASS = "";
     
 }
