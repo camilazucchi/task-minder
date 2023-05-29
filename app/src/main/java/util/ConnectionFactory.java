440bb33dfaf9ce90535b4ac81aea59199d15609d
Camila Zucchi <camilazucchi@outlook.com>
2023-05-29 14:18:15 -0300

    build: add mysql connector/j dependency

--- app/src/main/java/util/ConnectionFactory.java
+++ app/src/main/java/util/ConnectionFactory.java
@@ -2,9 +2,14 @@ package util;
 
 public class ConnectionFactory {
     
-    public static final String DRIVER = "com.mysql.j";
-    public static final String URL = "";
+    // Variável que especifica o uso do banco de dados MySQL:
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    // Variável do caminho de conexão até nosso banco de dados:
+    public static final String URL = "jdbc:mysql://localhost:3306/taskminder";
+    // Variável que especifica qual usuário será usado:
     public static final String USER = "root";
+    // Variável de senha:
     public static final String PASS = "";
+    
     
 }
