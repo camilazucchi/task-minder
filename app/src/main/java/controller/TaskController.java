87818d8a265c88d8f1eab3a4ab1e28302f29af38
Camila Zucchi <camilazucchi@outlook.com>
2023-05-30 13:30:03 -0300

    feat: add save method for task management

--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -8,22 +8,48 @@ import model.Task;
 import util.ConnectionFactory;
 
 public class TaskController {
-    
+
     public void save(Task task) {
-        
-    }
-    
-    public void update(Task task) {
-        
-    }
-    
-    public void removeById(int taskId) throws SQLException {
-        
-        String sql = "DELETE FROM tasks WHERE id = ?";
-        
+
+        String sql = "INSERT INTO tasks(idProject,"
+                + "name,"
+                + "description,"
+                + "completed,"
+                + "notes,"
+                + "deadline,"
+                + "createdAt,"
+                + "updatedAt)"
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
         Connection conn = null;
         PreparedStatement statement = null;
-        
+
+        try {
+            conn = ConnectionFactory.getConnection();
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, task.getIdProject());
+            statement.setString(2, task.getName());
+            statement.setString(3, task.getDescription());
+            statement.setBoolean(4, task.isIsCompleted());
+            statement.setString(5, task.getNotes());
+            statement.setDate(6, task.getDeadline());
+            statement.setDate(7, task.getCreatedAt());
+            statement.setDate(8, task.getUpdatedAt());
+        } catch (SQLException ex) {
+        }
+    }
+
+    public void update(Task task) {
+
+    }
+
+    public void removeById(int taskId) throws SQLException {
+
+        String sql = "DELETE FROM tasks WHERE id = ?";
+
+        Connection conn = null;
+        PreparedStatement statement = null;
+
         try {
             conn = ConnectionFactory.getConnection();
             statement = conn.prepareStatement(sql);
@@ -36,7 +62,7 @@ public class TaskController {
             ConnectionFactory.closeConnection(conn);
         }
     }
-    
+
     public List<Task> getAll(int idProject) {
         return null;
     }
