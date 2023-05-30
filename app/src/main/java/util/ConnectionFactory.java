65e724c1b7d27aba55bc6b0f2da634a04d8c3d64
Camila Zucchi <camilazucchi@outlook.com>
2023-05-30 14:00:56 -0300

    feat: add close statement to task handling

--- app/src/main/java/util/ConnectionFactory.java
+++ app/src/main/java/util/ConnectionFactory.java
@@ -2,6 +2,7 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
@@ -21,7 +22,7 @@ public class ConnectionFactory {
         try {
             Class.forName(DRIVER);
             return DriverManager.getConnection(URL, USER, PASS);
-        } catch (ClassNotFoundException | SQLException ex) {
+        } catch(ClassNotFoundException | SQLException ex) {
             throw new RuntimeException("Erro na conexão com o banco de dados: ", ex);
         }
     }
@@ -33,7 +34,22 @@ public class ConnectionFactory {
             if (connection != null) {
                 connection.close();
             }
-        } catch (SQLException ex) {
+        } catch(SQLException ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados: ", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection connection, PreparedStatement statement) {
+        try {
+            // Essa conexão existe? Se diferente de null = sim. Feche a conexão com connection.close:
+            if(connection != null) {
+                connection.close();
+            }
+            
+            if(statement != null) {
+                statement.close();
+            }
+        } catch(SQLException ex) {
             throw new RuntimeException("Erro ao fechar a conexão com o banco de dados: ", ex);
         }
     }
