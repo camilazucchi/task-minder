f91916336ae0519cf4ec240fabef002494bf1c4f
Camila Zucchi <camilazucchi@outlook.com>
2023-06-07 14:14:33 -0300

    feat: create TaskTableModel class

---
+++ app/src/main/java/util/TaskTableModel.java
@@ -0,0 +1,22 @@
+package util;
+
+import javax.swing.table.AbstractTableModel;
+
+public class TaskTableModel extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
