6eafef16a6bebff95b74e3c5f7c8f6ddefc23378
Camila Zucchi <camilazucchi@outlook.com>
2023-06-12 14:12:41 -0300

    feat: add TaskTableModel class with table model implementation

--- app/src/main/java/util/TaskTableModel.java
+++ app/src/main/java/util/TaskTableModel.java
@@ -1,22 +1,44 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.AbstractTableModel;
+import model.Task;
 
 public class TaskTableModel extends AbstractTableModel {
 
+    // Colunas do componente gráfico Table:
+    String[] columns = {"Name", "Description", "Deadline", "Task completed", "Edit", "Delete"};
+    // Lista que guarda tarefas:
+    List<Task> tasks = new ArrayList();
+
     @Override
+    // Linhas:
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return tasks.size();
     }
 
     @Override
+    // Colunas:
     public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return columns.length;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        switch (columnIndex) {
+            case 1:
+                return tasks.get(rowIndex).getName();
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+        }
+        return null;
     }
-    
 }
