24f271c386a74ed5d82e15a0912a97fb005b03f5
Camila Zucchi <camilazucchi@outlook.com>
2023-06-13 14:14:48 -0300

    feat: add getColumnName method to retrieve column name in TaskTableModel

--- app/src/main/java/util/TaskTableModel.java
+++ app/src/main/java/util/TaskTableModel.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
@@ -17,6 +18,11 @@ public class TaskTableModel extends AbstractTableModel {
     public int getRowCount() {
         return tasks.size();
     }
+    
+    @Override
+    public String getColumnName(int columnIndex) {
+        return columns[columnIndex];
+    }
 
     @Override
     // Colunas:
@@ -27,17 +33,18 @@ public class TaskTableModel extends AbstractTableModel {
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         switch (columnIndex) {
-            case 1:
+            case 0:
                 return tasks.get(rowIndex).getName();
-            case 2:
+            case 1:
                 return tasks.get(rowIndex).getDescription();
+            case 2:
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                return dateFormat.format(tasks.get(rowIndex).getDeadline());
             case 3:
-                return tasks.get(rowIndex).getDeadline();
-            case 4:
                 return tasks.get(rowIndex).isCompleted();
-            case 5:
+            case 4:
                 return "";
-            case 6:
+            case 5:
                 return "";
             default:
                 return "Not found.";
